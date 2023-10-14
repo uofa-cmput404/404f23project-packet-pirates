712230aedbdf321f1a0857cbe344d1af2b52770e
kj <leekaijie0805@gmail.com>
2023-10-14 14:46:35 -0600

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Landing from "./components/login/Login";
+import Landing from "./components/login/Landing";
 
 function App() {
   const [count, setCount] = useState(0);
