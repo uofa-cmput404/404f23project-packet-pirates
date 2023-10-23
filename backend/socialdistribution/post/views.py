47a6af0aadcbca29fe7a991f7bc94081aa248408
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-23 17:23:07 -0600

    Created serializer

--- backend/socialdistribution/post/views.py
+++ backend/socialdistribution/post/views.py
@@ -1,3 +1,14 @@
+from .serializer import AuthorSerializer
+# from .models import Author
+
 from django.shortcuts import render
+from django.contrib.auth import get_user_model, login, logout
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework import permissions, status
+
+from .serializer import *
 
 # Create your views here.
