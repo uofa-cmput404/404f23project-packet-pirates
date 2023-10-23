10f0a49b4003f03466e0bc3cda6cd02bcc2b5934
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-23 00:37:15 -0600

    Added staticfiles_dir in settings incase

--- backend/socialdistribution/socialdistribution/urls.py
+++ backend/socialdistribution/socialdistribution/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login.urls')),
-]+ static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # Don't know if we need this
+]
+# ]+ static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # Don't know if we need this
