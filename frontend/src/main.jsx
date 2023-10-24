034d5108d58728e842675b80c62f0c4c1a33affa
J-101 <jkhennig@ualberta.ca>
2023-10-24 01:10:57 -0600

    Notifications tab

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import Landing from "./components/login/Login.jsx";
+import Landing from "./components/login/Login.jsx";
 import MainPage from "./components/site/Site.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
