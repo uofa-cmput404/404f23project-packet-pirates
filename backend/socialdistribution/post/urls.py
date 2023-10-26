6c3888a4dc1d281501daae1b52bdaec6cad26b9b
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-26 15:44:52 -0600

    Update urls.py

--- backend/socialdistribution/post/urls.py
+++ backend/socialdistribution/post/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "post"
 urlpatterns = [
-    path("author/<str:pk>/post", views.GetPost.as_view(), name = "get_post")
+    path("author/<str:pk>/post", views.GetPost.as_view(), name = "get_post"),
+    path('createpost', views.CreatePost.as_view(), name="createpost"),
     # path("user/", views.GetPost.as_view(), name = "get_post")
 ]
