337f81b10acce3c958734e450078917c2e382f31
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-26 16:29:14 -0600

    Updated Post Model and DB

--- backend/socialdistribution/post/models.py
+++ backend/socialdistribution/post/models.py
@@ -31,7 +31,9 @@ class Post(models.Model):
 
     date_time = models.DateTimeField(auto_now_add=True, null = True, blank = True)
 
-    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    image_file = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    image_url = models.CharField(max_length=200, null=True, blank=True)
 
     unlisted = models.BooleanField(default=False)
 
