0e1ce09fff7a8cc64be9f582960a87ba81ee7760
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-26 15:45:32 -0600

    Update serializer.py

--- backend/socialdistribution/post/serializer.py
+++ backend/socialdistribution/post/serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import *
+from login.models import *
 from django.contrib.auth import get_user_model, authenticate
 from django.core.exceptions import ValidationError
+AuthorModel = get_user_model()
 
 class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.PrimaryKeyRelatedField(queryset=AppAuthor.objects.all(), pk_field = serializers.UUIDField(format ='hex_verbose'))
+    # author = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Post
         fields = "__all__"
