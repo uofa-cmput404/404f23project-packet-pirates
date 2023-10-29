c4fdeb2c4b460009ee70ab16dba33ba1decd72e8
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-10-28 22:17:04 -0600

    Updated Notification Model

--- backend/socialdistribution/feed/models.py
+++ backend/socialdistribution/feed/models.py
@@ -21,11 +21,20 @@ class Notifications(models.Model):
     # Need two authors
     # Also need FollowRequests
     # Posts, Likes, Comments
-    author = models.ForeignKey(AppAuthor, on_delete=models.CASCADE) # Main user
+    author = models.ForeignKey(AppAuthor, on_delete=models.CASCADE, related_name="main_author") # Main user
 
-    # Set symmetrical to false since we don't want it to work both ways. 
-    posts_liked = models.ManyToManyField(Post, symmetrical=False, blank = True)
-    post_comments = models.ManyToManyField(Comment, symmetrical=False, blank = True)
-    likes = models.ManyToManyField(PostLike, symmetrical=False, blank = True)
-    friend_requests = models.ManyToManyField(FollowerRequest, symmetrical=False, blank = True)
+    notification_author = models.ForeignKey(AppAuthor, on_delete=models.CASCADE, related_name="notifier") # Person who likes/comments
+
+    messages = [('liked your post', 'liked'), ('commented on your post', 'commented')]
+    message = models.CharField(max_length=200,choices=messages)
+
+    url = models.URLField(max_length = 300, null=True, blank=True) # URL of post
+
+    # # Set symmetrical to false since we don't want it to work both ways. 
+    # posts_liked = models.ManyToManyField(Post, symmetrical=False, blank = True)
+    # post_comments = models.ManyToManyField(Comment, symmetrical=False, blank = True)
+    # likes = models.ManyToManyField(PostLike, symmetrical=False, blank = True)
+    # friend_requests = models.ManyToManyField(FollowerRequest, symmetrical=False, blank = True)
+
+
 
