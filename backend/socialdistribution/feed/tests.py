122ea8c2914dd3d3bcb8a92fca7d1fcb3ce4de16
Jack Geiger <jggeiger@cs.ualberta.ca>
2023-10-30 13:51:31 -0600

    feed tests

--- backend/socialdistribution/feed/tests.py
+++ backend/socialdistribution/feed/tests.py
@@ -29,23 +29,79 @@ class FeedTests(TestCase):
         trueFriends1 = Friends.objects.create(author=author1, friend= author2, friend_pfp = None, friend_username = author2.username)
         trueFriends2 = Friends.objects.create(author=author2, friend= author1, friend_pfp = None, friend_username = author1.username)
 
+        friend1 = Friends.objects.create(author=author1, friend= author3, friend_pfp = None, friend_username = author3.username)
+        friend2 = Friends.objects.create(author=author2, friend= author3, friend_pfp = None, friend_username = author3.username)
+
         
     # True Friend Exists
     def testTrueFriendsExist(self):
-        pass
+        
+        self.client.login(username='CMPUT404', password='cmput404')
+        
+        author = AppAuthor.objects.get(username = 'Tester1', password="PassTester1")
+
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author.user_id) + '/truefriends')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Friends']), 1)
+
 
     #Test Get all Authors that an Author Follows
     def testAuthorFollows(self):
-        pass
+
+        self.client.login(username='CMPUT404', password='cmput404')
+        
+        author1 = AppAuthor.objects.get(username = 'Tester1', password="PassTester1")
+
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author1.user_id) + '/authorfollowing')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Friends']), 1)
+
+
+        author2 = AppAuthor.objects.get(username = 'Tester3', password="PassTester3")
+
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author2.user_id) + '/authorfollowing')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Friends']), 2)
+
 
     #Test Get all Authors following an Author
     def testAllAuthorsFollowingAuthor(self):
-        pass
+
+        self.client.login(username='CMPUT404', password='cmput404')
+        
+        author1 = AppAuthor.objects.get(username = 'Tester1', password="PassTester1")
+
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author1.user_id) + '/authorfollowers')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Friends']), 2)
+
+
+        author2 = AppAuthor.objects.get(username = 'Tester3', password="PassTester3")
+
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author2.user_id) + '/authorfollowers')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Friends']), 0)
+
 
     #Test if Notifications Exist
     def testNotificationsExist(self):
-        pass
+        
+        self.client.login(username='CMPUT404', password='cmput404')
+        
+        author1 = AppAuthor.objects.get(username = 'Tester1', password="PassTester1")
 
-    #Test AllAuthorsFriends()
-    def AllAuthorsFriends(self):
-        pass
+        response = self.client.get('http://127.0.0.1:8000/api/author/' + str(author1.user_id) + '/authornotifications')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.data['Notifications']), 1)
