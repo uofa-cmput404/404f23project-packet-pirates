fba4aa032f1d97f5ec2cb9d25110c16cfea0296a
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-20 21:02:27 -0700

    Updated origins

--- backend/socialdistribution/socialdistribution/settings.py
+++ backend/socialdistribution/socialdistribution/settings.py
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = ["*"]
 
 # CORS_ORIGIN_ALLOW_ALL = True
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://packet-pirates-frontend-46271456b73c.herokuapp.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
