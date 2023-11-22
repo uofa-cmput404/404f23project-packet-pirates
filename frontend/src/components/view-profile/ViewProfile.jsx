7a45199f1231cc9b840cd25740c747fad878e237
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-21 21:02:23 -0700

    Update ViewProfile.jsx

--- frontend/src/components/view-profile/ViewProfile.jsx
+++ frontend/src/components/view-profile/ViewProfile.jsx
@@ -104,7 +104,7 @@ export default function ViewProfile({ user }) {
                 user={user}
                 title={post.title}
                 description={post.content}
-                img={post.image_url}
+                img={post.image_url} // Change this to image_file
                 likes={post.likes_count}
                 id={post.post_id}
               />
