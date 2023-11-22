fc8f9986cb421997e1707c0e3355b3eaf3265050
J-101 <jkhennig@ualberta.ca>
2023-11-21 23:12:23 -0700

    Created top profile bar template on profile pages

--- frontend/src/components/view-profile/ViewProfile.jsx
+++ frontend/src/components/view-profile/ViewProfile.jsx
@@ -143,44 +143,69 @@ export default function ViewProfile({ user }) {
   return (
     <>
       <div className="flex justify-center items-center w-screen">
-        <div className="main w-full max-w-[70rem] flex flex-row justify-center m-7">
-          <div
-            className="profile h-fit mx-auto"
-            style={{ position: "sticky", top: "20px" }}
-          >
-            {/* <Profile friends={friends} username={user.user.username} /> */}
-            {friends}
+        <div className="main w-full max-w-[70rem] flex flex-col justify-center items-center m-7">
+          <div className="top-box bg-gray-200 p-4 mb-4 text-center rounded-md flex flex-col items-center w-full">
+            {/* User's Profile Picture */}
+            <img
+              src={user.user.profile_picture} // Assuming the user object has a profile_picture property
+              alt={`${user.user.username}'s Profile`}
+              className="w-12 h-12 rounded-full object-cover mb-4"
+            />
+  
+            {/* User's Name */}
+            <h2 className="text-xl font-semibold mb-2">{user.user.username}</h2>
+  
+            {/* Follow Button */}
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              Follow
+            </button>
           </div>
-          <div className="feed flex flex-col ml-5 w-full mx-auto">
-            <div className="flex items-center justify-center">
-              You are looking at {author}'s profile
+  
+          {/* Profile Header Section */}
+          {profile && (
+            <div className="profile-header mt-4 p-4 border border-gray-300 rounded-md text-center">
+              <img
+                src={profile.profile_picture}
+                alt={`${profile.username}'s Profile`}
+                className="w-16 h-16 rounded-full object-cover mb-2"
+              />
+              <h2 className="text-xl font-semibold">{profile.username}</h2>
+              {/* Add more details about the profile here if needed */}
+              <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2">
+                Follow
+              </button>
             </div>
-            <div className="feed_content mt-5">
-              <ul>{posts}</ul>
-            </div>
-          </div>
-          {/* <div
-            className="notifications h-fit mx-auto ml-5"
-            style={{ position: "sticky", top: "20px" }}
-          >
-            {notifications}
-          </div> */}
-          <div className="flex-col justify-center mx-4">
-            <div className="search-bar">
-              <SearchBar />
-            </div>
+          )}
+  
+          <div className="flex flex-row w-full mx-auto">
             <div
-              className="notifications h-fit mx-auto"
+              className="profile h-fit mx-auto"
               style={{ position: "sticky", top: "20px" }}
             >
-              {notifications}
+              {friends}
             </div>
-            <button
-              onClick={handleLogout}
-              className="sticky top-[270px] block rounded-lg text-white bg-primary-dark w-3/5 mx-auto my-4 py-2 shadow-md hover:bg-primary-color transition duration-200 ease-in"
-            >
-              Logout
-            </button>
+            <div className="feed flex flex-col ml-5 w-full mx-auto">
+              <div className="feed_content mt-5">
+                <ul>{posts}</ul>
+              </div>
+            </div>
+            <div className="flex-col justify-center mx-4">
+              <div className="search-bar">
+                <SearchBar />
+              </div>
+              <div
+                className="notifications h-fit mx-auto"
+                style={{ position: "sticky", top: "20px" }}
+              >
+                {notifications}
+              </div>
+              <button
+                onClick={handleLogout}
+                className="sticky top-[270px] block rounded-lg text-white bg-primary-dark w-3/5 mx-auto my-4 py-2 shadow-md hover:bg-primary-color transition duration-200 ease-in"
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       </div>
