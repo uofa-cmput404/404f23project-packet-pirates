3b6ecb644f7a4ad66c1f455b34c905055b2426f6
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-22 15:12:00 -0700

    Fixed NotLoggedin displaying unlisted posts

--- frontend/src/components/view-profile/ViewProfileNotLoggedIn.jsx
+++ frontend/src/components/view-profile/ViewProfileNotLoggedIn.jsx
@@ -50,7 +50,7 @@ export default function ViewProfileNotLogged() {
           // console.log("POSTSRES_FULL", postsRes.data.Posts);
           console.log("POSTSRES", postsRes.status);
           setPosts(
-            postsRes.data.Posts.filter((post) => !post.is_private).map((post, index) => {
+            postsRes.data.Posts.filter((post) => !post.unlisted && !post.is_private).map((post, index) => {
               const image_conditions = post.image_url === null && post.image_file != null
               // console.log("TESTING", image_conditions)
               const image = image_conditions ? 'http://127.0.0.1:8000' + post.image_file : post.image_url
