b47bc6931039981c39983f26c6684a8b91ad80cc
J-101 <jkhennig@ualberta.ca>
2023-11-22 21:42:57 -0700

    Checkmark and 'X' added to notifs

--- frontend/src/components/main-feed/Notifications.jsx
+++ frontend/src/components/main-feed/Notifications.jsx
@@ -10,29 +10,49 @@ export default function Notifications({ notifications }) {
       <div className="notifications">
         <ul>
           {notifications.map((notification, index) => (
-            <li
+            <FollowNotification
               key={index}
-              className="notification flex flex-row list-image-none justify-start items-center py-4 w-full"
-            >
-              <div className="image-container w-10 h-10 rounded-full overflow-hidden bg-black">
-                <img
-                  src={notification.notif_author_pfp}
-                  alt="Profile"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <div className="notification-content flex flex-col ml-5">
-                <span className="border border-[#A5C9CA] bg-[#A5C9CA] w-fit pl-3 pr-3 text-black rounded-full whitespace-nowrap">
-                  {notification.notif_author_username}
-                </span>
-                <span className="mt-1 text-sm text-gray-500 whitespace-nowrap">
-                  {notification.message}
-                </span>
-              </div>
-            </li>
+              index={index}
+              notification={notification}
+            />
           ))}
         </ul>
       </div>
     </button>
+  );
+}
+
+export function FollowNotification({ index, notification }) {
+  return (
+    <li
+      key={index}
+      className="notification flex flex-row list-image-none justify-start items-center py-4 w-full"
+    >
+      <div className="image-container w-10 h-10 rounded-full overflow-hidden bg-black">
+        <img
+          src={notification.notif_author_pfp}
+          alt="Profile"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="notification-content flex flex-col ml-5">
+        <div className="top flex flex-row justify-between items-center">
+          <span className="border border-[#A5C9CA] bg-[#A5C9CA] w-fit pl-3 pr-3 text-black rounded-full whitespace-nowrap">
+            {notification.notif_author_username}
+          </span>
+          <div className="buttons flex flex-row">
+            <button className="bg-primary-color text-white rounded-lg px-1 py-1 mx-1">
+              ✔️
+            </button>
+            <button className="bg-primary-color text-white rounded-lg px-1 py-1 mx-1">
+              ❌
+            </button>
+          </div>
+        </div>
+        <span className="mt-1 text-sm text-gray-500 whitespace-nowrap">
+          {notification.message}
+        </span>
+      </div>
+    </li>
   );
 }
