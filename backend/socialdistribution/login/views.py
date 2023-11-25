ec987dc6da717e0f61970d42206deea648f9dcb4
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-25 13:04:45 -0700

    Updated Author Remote Serializer

--- backend/socialdistribution/login/views.py
+++ backend/socialdistribution/login/views.py
@@ -20,6 +20,8 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import DefaultStorage, FileSystemStorage
 from django.core.files import File
 from django.core.files.images import ImageFile
+from django.core.paginator import Paginator
+
 from django.conf import settings
 
 from urllib.request import urlopen
@@ -28,6 +30,8 @@ from PIL import Image
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+
+
 
 class AuthorRegistration(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -173,38 +177,38 @@ class getAllAuthorsRemote(APIView):
         size: how big is a page
     '''
 
-    permission_classes = (permissions.IsAuthenticated, )
-    authentication_classes = (BasicAuthentication, )
+    # permission_classes = (permissions.IsAuthenticated, )
+    # authentication_classes = (BasicAuthentication, )
 
     @swagger_auto_schema(operation_description="Get All Authors Remote",
             operation_summary="Get All Authors Remote",
-            responses={200: AuthorSerializer()},
+            responses={200: AuthorSerializerRemote()},
             tags=['Remote'],)
     
     def get(self, request):
         all_authors = AppAuthor.objects.all()
         
-        serializer = AuthorSerializer(all_authors, many = True)
+        serializer = AuthorSerializerRemote(all_authors, many = True)
 
-        return Response({"items": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"type" : "authors", "items": serializer.data}, status=status.HTTP_200_OK)
 
 class getSingleAuthorRemote(APIView):
     '''
     URL: ://service/authors/{AUTHOR_ID}/
     GET [local, remote]: retrieve AUTHOR_ID’s profile
     '''
-    permission_classes = (permissions.IsAuthenticated, )
-    authentication_classes = (BasicAuthentication, )
+    # permission_classes = (permissions.IsAuthenticated, )
+    # authentication_classes = (BasicAuthentication, )
 
     @swagger_auto_schema(operation_description="Get A Single Author Remote",
             operation_summary="Get a single author remote",
-            responses={200: AuthorSerializer()},
+            responses={200: AuthorSerializerRemote()},
             tags=['Remote'],)
     
     def get (self, request, author_id):
         author = AppAuthor.objects.get(user_id = author_id)
 
-        serializer = AuthorSerializer(author)
-
+        serializer = AuthorSerializerRemote(author)
+        print("made it")
         return Response (serializer.data, status=status.HTTP_200_OK)
     
