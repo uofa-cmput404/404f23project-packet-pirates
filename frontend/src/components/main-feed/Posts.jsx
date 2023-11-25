f20929cd6fbde501e99042e21299e46c4488ab32
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-24 21:52:26 -0700

    Update Posts.jsx

--- frontend/src/components/main-feed/Posts.jsx
+++ frontend/src/components/main-feed/Posts.jsx
@@ -112,13 +112,15 @@ export default function Post({
     .get(authorUrl,config)
     .then(async (authorRes) => {
 
-      await axios.post(commentsUrl, config, { 
+      await axios.post(commentsUrl, { 
           text: commentText,
           author: user.user.user_id,
           author_picture: "https://packet-pirates-backend-d3f5451fdee4.herokuapp.com" + authorRes.data.Author.profile_picture,
           author_username: authorRes.data.Author.username,
         
-      },)
+      }, config, {
+        withCredentials: true
+      })
       .then(() => {
 
         getComments()
