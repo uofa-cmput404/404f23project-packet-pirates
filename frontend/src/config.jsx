d06f9046c4d21581030bfd053f1f143ca98dedac
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-27 12:22:42 -0700

    Update config.jsx

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,7 +1,7 @@
 // export const API_ENDPOINT = "http://127.0.0.1:8000/";
 
 export const config = {
-  API_ENDPOINT: "http://127.0.0.1:8000/",
+  API_ENDPOINT: "https://packet-pirates-backend-d3f5451fdee4.herokuapp.com/",
   SUPER_CODING_ENDPOINT:
     "https://super-coding-team-89a5aa34a95f.herokuapp.com/service/",
   NODE_NET_ENDPOINT: "https://node-net-46d70235bc29.herokuapp.com/api/",
