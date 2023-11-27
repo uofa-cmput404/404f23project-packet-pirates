a96aac57d92c7caab3c0fd66faa8fe03bc44b9fd
Jack Geiger <jggeiger@cs.ualberta.ca>
2023-11-26 20:33:24 -0700

    inbox posts

--- frontend/src/components/main-feed/Inbox.jsx
+++ frontend/src/components/main-feed/Inbox.jsx
@@ -11,74 +11,78 @@ export default function Inbox({ user }) {
 
   console.log(user);
 
+  const fetchPostData = async () => {
+
+    try {
+      // console.log("FETCHING POST DATA AT URL:", post.API);
+      // const response = await axios.get(post.API)
+      const response = await axios
+        .get(
+          "http://127.0.0.1:8000/author/" + user.user.user_id + "/inbox/local/posts"
+        )
+
+        .then((res) => {
+          console.log("res", res);
+
+          setShowPost(() => [
+            res.data.map((post, index) => {
+              return (
+                <RemotePost
+                  key={index}
+                  user={user}
+                  title={post.title}
+                  description={post.description}
+                  content={post.content}
+                  img={post.image_url}
+                  likes={post.likes_count}
+                />
+              );
+            }),
+          ]);
+
+        });
+
+    } catch (error) {
+
+      console.error("Error fetching post data:", error);
+      console.log("APDIASPODIASPDIAPSDIPASOID" + post.API);
+      throw error; // Rethrow the error to be caught by Promise.all
+
+    }
+  };
+
   useEffect(() => {
     getInbox();
   }, []);
 
   useEffect(() => {
-    console.log("inbox111", inbox);
-    const posts = Object.values(inbox.posts || {});
-    const postComments = inbox.post_comments || {};
-    const postLikes = inbox.post_likes || {};
-    const followRequests = inbox.follow_requests || {};
-    const notifications = inbox.notifications || [];
-    const fetchPostData = async (post) => {
-      try {
-        // console.log("FETCHING POST DATA AT URL:", post.API);
-        // const response = await axios.get(post.API)
-        const response = await axios
-          .get(
-            // "http://127.0.0.1:8000/authors/5e4b8ac6-c5bb-4a4c-b671-a76300236f5d/posts/50d4afaf39de4c8f9cfe0f1f6421d7bd"
-          "http://127.0.0.1:8000/author/5e4b8ac6-c5bb-4a4c-b671-a76300236f5d/inbox/local/posts"
 
-          )
-          .then((res) => {
-            console.log("res", res);
-            setShowPost((prev) => [
-              ...prev,
-              res.data.map((post, index) => {
-                return (
-                  <RemotePost
-                    key={index}
-                    user={user}
-                    title={post.title}
-                    description={post.description}
-                    content={post.content}
-                    img={post.image_url}
-                    likes={post.likes_count}
-                  />
-                );
-              }),
-            ]);
-          });
-      } catch (error) {
-        console.error("Error fetching post data:", error);
-        console.log("APDIASPODIASPDIAPSDIPASOID" + post.API);
-        throw error; // Rethrow the error to be caught by Promise.all
-      }
-    };
+    //console.log("inbox111", inbox);
+    //const posts = Object.values(inbox.posts || {});
+    //const postComments = inbox.post_comments || {};
+    //const postLikes = inbox.post_likes || {};
+    //const followRequests = inbox.follow_requests || {};
+    //const notifications = inbox.notifications || [];
 
-    // posts.forEach((post) => {
-    //   console.log("post", post);
-    //   fetchPostData(post);
-    // });
+    //console.log("posts", posts);
+    //console.log("postComments", postComments);
+    //console.log("postLikes", postLikes);
+    //console.log("followRequests", followRequests);
+    //console.log("notifications", notifications);
+    //console.log("inboxPosts", inboxPosts);
 
-    fetchPostData(posts)
-
+    fetchPostData()
     setPostsFetched(true);
-    console.log("posts", posts);
-    console.log("postComments", postComments);
-    console.log("postLikes", postLikes);
-    console.log("followRequests", followRequests);
-    console.log("notifications", notifications);
-    console.log("inboxPosts", inboxPosts);
-  }, [inbox, postsFetched]);
 
-  const getInbox = () => {
+  }, [inbox]);
+
+  const getInbox = async () => {
+
     console.log("getting inbox: ", user);
-    axios
+
+    await axios
       .get(
-        config.API_ENDPOINT + "/author/" + user.user.user_id + "/inbox/local"
+        config.API_ENDPOINT + "author/" + user.user.user_id + "/inbox/local"
       )
       .then((res) => {
         console.log("inbox", res);
@@ -90,27 +94,28 @@ export default function Inbox({ user }) {
       });
   };
 
-  const testApi = () => {
-    console.log("test");
-    console.log(user);
-    axios
-      .get(config.NODE_NET_ENDPOINT + "authors/", {
-        auth: {
-          username: "Pirate",
-          password: "Pirate",
-        },
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log("Request succeeded", res.data);
-      })
-      .catch((err) => {
-        console.error("Request failed", err);
-      });
-    console.log("test2");
+  // const testApi = () => {
+  //   console.log("test");
+  //   console.log(user);
+  //   axios
+  //     .get(config.NODE_NET_ENDPOINT + "authors/", {
+  //       auth: {
+  //         username: "Pirate",
+  //         password: "Pirate",
+  //       },
+  //       withCredentials: true,
+  //     })
+  //     .then((res) => {
+  //       console.log("Request succeeded", res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.error("Request failed", err);
+  //     });
+  //   console.log("test2");
 
-    getInbox();
-  };
+  //   getInbox();
+  // };
+
   return (
     <>
       <div className="container flex flex-col">
