fd12094dae61f49457a6a2450fd7d8e084202c11
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-27 12:53:17 -0700

    Update settings.py

--- backend/socialdistribution/socialdistribution/settings.py
+++ backend/socialdistribution/socialdistribution/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-COR_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
 'http://127.0.0.1:3000', 'http://localhost:3000', 
 'https://packet-pirates-frontend-46271456b73c.herokuapp.com/', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com/',
 'https://packet-pirates-frontend-46271456b73c.herokuapp.com', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com',
