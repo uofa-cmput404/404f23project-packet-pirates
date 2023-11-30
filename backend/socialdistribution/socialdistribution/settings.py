f56656ae8c28b13a1b561908c8f7224b97a433f2
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-11-30 16:20:25 -0700

    Update settings.py

--- backend/socialdistribution/socialdistribution/settings.py
+++ backend/socialdistribution/socialdistribution/settings.py
@@ -33,12 +33,12 @@ CORS_ALLOWED_ORIGINS = [
 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:8000', 'http://localhost:8000',
 'https://packet-pirates-frontend-46271456b73c.herokuapp.com', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com',
 'https://packet-pirates-frontend-46271456b73c.herokuapp.com', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com',
-'https://super-coding-team-89a5aa34a95f.herokuapp.com']
+'https://super-coding-team-89a5aa34a95f.herokuapp.com', 'https://node-net-46d70235bc29.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000', 
                         'https://packet-pirates-frontend-46271456b73c.herokuapp.com/', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com/',
                         'https://packet-pirates-frontend-46271456b73c.herokuapp.com', 'https://packet-pirates-backend-d3f5451fdee4.herokuapp.com',
-                        'https://super-coding-team-89a5aa34a95f.herokuapp.com/']
+                        'https://super-coding-team-89a5aa34a95f.herokuapp.com/', 'https://node-net-46d70235bc29.herokuapp.com/']
 
 CORS_ALLOW_CREDENTIALS = True
 
