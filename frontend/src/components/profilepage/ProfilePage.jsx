38b6f0bc1da98ad2b3f0f5c600f26e52830491d8
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-12-07 16:36:42 -0700

    Updating profile saving

--- frontend/src/components/profilepage/ProfilePage.jsx
+++ frontend/src/components/profilepage/ProfilePage.jsx
@@ -121,7 +121,7 @@ export default function ProfilePage({ user }) {
                                     onChange={handleGitChange}
                                 />
                             
-                                <label
+                                {/* <label
                                     htmlFor="displayname"
                                     className="block ml-10 row-span-1 col-start-1 col-end-2 text-sm font-medium text-lm-custom-black dark:text-white"
                                 >
@@ -134,7 +134,7 @@ export default function ProfilePage({ user }) {
                                     placeholder={user.user.username}
                                     className="bg-gray-50 border border-gray-300 row-span-1 col-start-2 col-end-3 text-lm-custom-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     onChange={handleDisplayNameChange}
-                                />
+                                /> */}
                             
                                 <button 
                                     onClick={()=>{navigate("/")}}
