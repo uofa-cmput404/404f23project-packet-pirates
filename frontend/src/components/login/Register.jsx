686b38c9e10c2c3991f1c2a5268d55588dda6779
ggiang1 <113879588+ggiang1@users.noreply.github.com>
2023-12-08 16:38:54 -0700

    Got rid of a register field

--- frontend/src/components/login/Register.jsx
+++ frontend/src/components/login/Register.jsx
@@ -155,7 +155,7 @@ export default function Register() {
                 />
               </div>
 
-              <div>
+              {/* <div>
                 <label
                   htmlFor="displayname"
                   className="block mb-2 text-sm font-medium text-lm-custom-black dark:text-white"
@@ -170,7 +170,7 @@ export default function Register() {
                   className="bg-gray-50 border border-gray-300 text-lm-custom-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={handleDisplayNameChange}
                 />
-              </div>
+              </div> */}
 
               <div>
                 <label htmlFor="select-image">
